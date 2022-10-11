1263d0e0413e5d165c18b15cbaba44ff6debf105
welsewul <klolita7878@gmail.com>
2022-10-11 19:36:15 -0400

    animation branch created

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -3,53 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class Script : MonoBehaviour
+public class CatController : MonoBehaviour
 {
     public AudioClip musicClipOne;
     public AudioClip musicClipTwo;
     public AudioSource musicSource;
-    // Start is called before the first frame update
+    Animator anim;
+    
     void Start()
+    
     {
 
+     anim = GetComponent<Animator>();
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.W))
+
         {
+
             musicSource.clip = musicClipOne;
+
             musicSource.Play();
+
+            anim.SetInteger("State", 1);
 
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.W))
+
         {
+
             musicSource.Stop();
+
+            anim.SetInteger("State", 0);
 
         }
 
         if (Input.GetKeyDown(KeyCode.R))
+
         {
+
             musicSource.clip = musicClipTwo;
+
             musicSource.Play();
+
+            anim.SetInteger("State", 2);
+
         }
 
         if (Input.GetKeyUp(KeyCode.R))
+
         {
+
             musicSource.Stop();
+
+            anim.SetInteger("State", 0);
 
         }
 
         if (Input.GetKeyDown(KeyCode.L))
+
         {
+
             musicSource.loop = true;
+
         }
 
         if (Input.GetKeyUp(KeyCode.L))
+
         {
+
             musicSource.loop = false;
+
         }
     }
 }
